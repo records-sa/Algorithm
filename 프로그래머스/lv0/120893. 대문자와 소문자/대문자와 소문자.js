3672fca82fb47c162cef5b045d001294ef257cc5
Seoa Kim <128363466+records-sa@users.noreply.github.com>
2023-06-22 12:50:05 +0900

    [level 0] Title: 대문자와 소문자, Time: 0.32 ms, Memory: 33.6 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120893. 대문자와 소문자/대문자와 소문자.js
@@ -0,0 +1,11 @@
+function solution(my_string) {
+    let result = '';
+    for (let i = 0; i < my_string.length; i++) {
+        if(my_string[i] === my_string[i].toUpperCase()) {
+            result += my_string[i].toLowerCase();
+        } else {
+            result += my_string[i].toUpperCase();
+        }
+    }
+    return result;
+}
