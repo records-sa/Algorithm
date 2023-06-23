5c38b8758824163920b4e276b705b0b1afdefff0
Seoa Kim <128363466+records-sa@users.noreply.github.com>
2023-06-23 12:16:45 +0900

    [unrated] Title: 과일 장수, Time: 0.09 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/unrated/135808. 과일 장수/과일 장수.js
+++ 프로그래머스/unrated/135808. 과일 장수/과일 장수.js
@@ -1,19 +1,17 @@
 function solution(k, m, score) {
+    // 1. score 배열 오름차순으로 정렬
     let arrScore = score.sort((a, b) => b - a);
+    // 2. m 만큼 나눠서 박스가 몇갠지 구하기
     let appleBox = Math.floor(arrScore.length / m);
+    // 3. 사과 박스에서 가장 낮은 점수(m-1 번째 인덱스) 찾아서 배열에 넣기
     let applePrice = [];
     for (i = 1; i <= appleBox; i++) {
         applePrice.push(arrScore[(m * i) - 1]);
     }
+    // 4. 이익이 발생하지 않는 경우 0 조건 넣기, 가격 합계 구하기
     if (appleBox === 0) {
         return 0;
     } else {
         return applePrice.reduce((sum, current) => sum + (current * m), 0);
     }
 }
-
-// 1. score 배열 오름차순으로 정렬
-// 2. m 만큼 나눠서 박스가 몇갠지 구하기
-// 3. 사과 박스에서 가장 낮은 점수(m-1 번째 인덱스) 찾기 
-// 4. 가격 합계 구하기
-// 5. 이익이 발생하지 않는 경우 0 조건 넣기
