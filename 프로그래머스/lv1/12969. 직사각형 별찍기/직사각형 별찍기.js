7bd9f9f16f817aec8ba619d0940ae7eb0215b6cc
Seoa Kim <128363466+records-sa@users.noreply.github.com>
2023-06-30 11:43:00 +0900

    [level 1] Title: 직사각형 별찍기, Time: 49.03 ms, Memory: 34.7 MB -BaekjoonHub

--- 프로그래머스/lv1/12969. 직사각형 별찍기/직사각형 별찍기.js
+++ 프로그래머스/lv1/12969. 직사각형 별찍기/직사각형 별찍기.js
@@ -8,3 +8,17 @@ process.stdin.on('data', data => {
     const print = arr.join('\n');
     console.log(print.replaceAll(',',''));
 });
+
+
+
+/* 다른 풀이
+process.stdin.setEncoding('utf8');
+process.stdin.on('data', data => {
+    const n = data.split(" ");
+    const a = Number(n[0]), b = Number(n[1]);
+    const row = '*'.repeat(a)
+    for(let i =0; i < b; i++){
+        console.log(row)
+    }
+
+}); */
