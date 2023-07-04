e6dd59753c10582e18a213f913345ddaa341235e
Seoa Kim <128363466+records-sa@users.noreply.github.com>
2023-07-04 12:13:10 +0900

    [level 1] Title: 최소직사각형, Time: 4.94 ms, Memory: 38.9 MB -BaekjoonHub

--- 프로그래머스/lv1/86491. 최소직사각형/최소직사각형.js
+++ 프로그래머스/lv1/86491. 최소직사각형/최소직사각형.js
@@ -14,3 +14,17 @@ function solution(sizes) {
     }
     return Math.max(...arrL) * Math.max(...arrS);
 }
+
+/* 다른 풀이
+function solution(sizes) {
+    let w = 0;
+    let h = 0;
+    sizes.forEach(s => {
+        const [a, b] = s.sort((a,b) => a-b);
+        if (a > h) h = a;
+        if (b > w) w = b;
+    });
+
+    return w * h;
+}
+*/
