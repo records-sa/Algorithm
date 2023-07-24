55a51636393b4857411455fb2b3efd681abb6dfe
Seoa Kim <128363466+records-sa@users.noreply.github.com>
2023-07-24 11:25:18 +0900

    [level 2] Title: 최댓값과 최솟값, Time: 0.11 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/12939. 최댓값과 최솟값/최댓값과 최솟값.js
@@ -0,0 +1,9 @@
+function solution(s) {
+    let answer = '';
+    const arrS = s.split(' ');
+    arrS.sort((a, b) => +a - +b);
+    answer += arrS[0] + '';
+    answer += ' ';
+    answer += arrS[arrS.length - 1] + '';
+    return answer;
+}
