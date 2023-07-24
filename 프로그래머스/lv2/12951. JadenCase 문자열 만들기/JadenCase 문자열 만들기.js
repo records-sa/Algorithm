d18c9ff210948d284e13c77e19dbb4e6b76b57f4
Seoa Kim <128363466+records-sa@users.noreply.github.com>
2023-07-24 11:58:15 +0900

    [level 2] Title: JadenCase 문자열 만들기, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.js
@@ -0,0 +1,17 @@
+function solution(s) {
+    const arrS = s.split(' ');
+    let toJadenCase = '';
+    for (let i = 0; i < arrS.length; i++) {
+        for (let j = 0; j < arrS[i].length; j++) {
+            if (j === 0) {
+                toJadenCase += arrS[i][j].toUpperCase();
+            } else {
+                toJadenCase += arrS[i][j].toLowerCase();
+            }
+        }
+        if (i < arrS.length - 1) {
+            toJadenCase += ' ';
+        }
+    }
+    return toJadenCase;
+}
