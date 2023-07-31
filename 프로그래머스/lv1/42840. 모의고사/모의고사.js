32e2e60738a29097d49c0b4301741bad7c33f8f8
Seoa Kim <128363466+records-sa@users.noreply.github.com>
2023-07-31 14:41:32 +0900

    [level 1] Title: 모의고사, Time: 3.25 ms, Memory: 36.8 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/42840. 모의고사/모의고사.js
@@ -0,0 +1,21 @@
+function solution(answers) {
+    let answer = [];
+    const soopoja1 = [1, 2, 3, 4, 5];
+    const soopoja2 = [2, 1, 2, 3, 2, 4, 2, 5];
+    const soopoja3 = [3, 3, 1, 1, 2, 2, 4, 4, 5, 5];
+    
+    let score = [0, 0, 0];
+    
+    for (let i = 0; i < answers.length; i++) {
+        if (answers[i] === soopoja1[i % 5]) score[0]++;
+        if (answers[i] === soopoja2[i % 8]) score[1]++;
+        if (answers[i] === soopoja3[i % 10]) score[2]++;
+    }
+    
+    const max = Math.max(...score);
+    
+    for (let j = 0; j < score.length; j++) {
+        if (max === score[j]) answer.push(j + 1);
+    }
+    return answer;
+}
