111c47e1732a651f7c1c9c934cac7a2a49f1df10
Seoa Kim <128363466+records-sa@users.noreply.github.com>
2023-08-21 14:44:03 +0900

    [unrated] Title: 배열의 원소 삭제하기, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181844. 배열의 원소 삭제하기/배열의 원소 삭제하기.js
@@ -0,0 +1,9 @@
+function solution(arr, delete_list) {
+    let answer = [];
+    for (let i = 0; i < arr.length; i++) {
+        if (!delete_list.includes(arr[i])) {
+            answer.push(arr[i]);
+        }
+    }
+    return answer;
+}
