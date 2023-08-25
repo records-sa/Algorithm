a6c1f97b32c5c984d23587662c3b0822eb8a9aa5
Seoa Kim <128363466+records-sa@users.noreply.github.com>
2023-08-25 12:48:39 +0900

    [level 0] Title: 문자열 정렬하기 (1), Time: 0.19 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120850. 문자열 정렬하기 （1）/문자열 정렬하기 （1）.js
@@ -0,0 +1,7 @@
+function solution(my_string) {
+    let answer = [];
+    for (let i = 0; i < my_string.length; i++) {
+        if(!isNaN(my_string[i])) answer.push(+my_string[i]);
+    }
+    return answer.sort();
+}
