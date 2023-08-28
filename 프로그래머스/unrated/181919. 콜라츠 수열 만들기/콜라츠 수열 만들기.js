919119e29dac3f6321a6ef6bf55c48f274809408
Seoa Kim <128363466+records-sa@users.noreply.github.com>
2023-08-28 11:59:12 +0900

    [unrated] Title: 콜라츠 수열 만들기, Time: 0.11 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181919. 콜라츠 수열 만들기/콜라츠 수열 만들기.js
@@ -0,0 +1,15 @@
+function solution(n) {
+    let answer = [];
+    do {
+        answer.push(n);
+        
+        if (n % 2 === 0) {
+            n = n / 2;
+        } else {
+            n = 3 * n + 1;
+        }
+    } while (n !== 1);
+    answer.push(n);
+    
+    return answer;
+}
