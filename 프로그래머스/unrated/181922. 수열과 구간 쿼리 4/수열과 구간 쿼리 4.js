cc0a81b4522b28134cb072f2dd6cc0ad55348268
Seoa Kim <128363466+records-sa@users.noreply.github.com>
2023-10-13 17:54:15 +0900

    [level unrated] Title: 수열과 구간 쿼리 4, Time: 3.65 ms, Memory: 36.8 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181922. 수열과 구간 쿼리 4/수열과 구간 쿼리 4.js
@@ -0,0 +1,11 @@
+function solution(arr, queries) {
+    for (let i = 0; i < queries.length; i++) {
+        const s = queries[i][0];
+        const e = queries[i][1];
+        const k = queries[i][2];
+        for (let j = s; j <= e; j++) {
+            if (j % k === 0) arr[j]++
+        }
+    }
+    return arr;
+}
