5caf5a7d6dd88cbfd1ecab130e3fb1d1dfff1b8b
Seoa Kim <128363466+records-sa@users.noreply.github.com>
2023-10-27 15:19:32 +0900

    [level 2] Title: 다음 큰 숫자, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/2/12911. 다음 큰 숫자/다음 큰 숫자.js
@@ -0,0 +1,17 @@
+function solution(n) {
+    function countOnes(binaryStr) {
+        return binaryStr.split('1').length - 1;
+    }
+    
+    const binaryN = n.toString(2);
+    const onesCount = countOnes(binaryN);
+    
+    while (true) {
+        n++;
+        const binaryNext = n.toString(2);
+        
+        if (countOnes(binaryNext) === onesCount) {
+            return n;
+        }
+    }
+}
